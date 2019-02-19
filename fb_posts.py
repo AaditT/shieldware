97099a84dd9f6e8703b945f629e97392ac917a9c
Aadit Trivedi <aadit.trivedi03@gmail.com>
2019-02-18 22:10:19 -0800

    Update fb_posts.py

--- fb_posts.py
+++ fb_posts.py
@@ -3,13 +3,8 @@
 import facebook
 import requests
 
-
 def some_action(post):
-    """ Here you might want to do something with each post. E.g. grab the
-    post's message (post['message']) or the post's picture (post['picture']).
-    In this implementation we just print the post's created time.
-    """
-    print(post["created_time"])
+    print(post["picture"])
 
 access_token = ""
 user = "BillGates"
@@ -18,9 +13,6 @@ graph = facebook.GraphAPI(access_token)
 profile = graph.get_object(user)
 posts = graph.get_connections(profile["id"], "posts")
 
-while True:
-    try:
-        [some_action(post=post) for post in posts["data"]]
-        posts = requests.get(posts["paging"]["next"]).json()
-    except KeyError:
-        break
+
+[some_action(post=post) for post in posts["data"]]
+posts = requests.get(posts["paging"]["next"]).json()
