3887a91b1ed0901c3a731646ba13b2ad79a18c0b
AaditT <aadit.trivedi03@gmail.com>
2019-03-02 13:55:55 -0800

    pretty sit

--- instagramdata.py
+++ instagramdata.py
@@ -1,12 +1,14 @@
 from instagram_web_api import Client, ClientCompatPatch, ClientError, ClientLoginError
 import itertools
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 web_api = Client(auto_patch=True, drop_incompat_keys=False)
 
 def get_all_igposts(id):
     posts = []
     user_feed_info = web_api.user_feed(id)
-    print(user_feed_info)
+    pp.pprint(user_feed_info)
     for post in user_feed_info:
         _url = post['node']['display_url']
     posts.append(_url)
