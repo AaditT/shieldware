d61b9cb00303c37cc20d3704f05dab5ed6b3badb
AaditT <aadit.trivedi03@gmail.com>
2019-03-02 13:54:05 -0800

    sit

--- instagramdata.py
+++ instagramdata.py
@@ -6,7 +6,7 @@ web_api = Client(auto_patch=True, drop_incompat_keys=False)
 def get_all_igposts(id):
     posts = []
     user_feed_info = web_api.user_feed(id)
-
+    print(user_feed_info)
     for post in user_feed_info:
         _url = post['node']['display_url']
     posts.append(_url)
