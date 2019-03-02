4e12b8cccae306ddbbecb63d5adb2e1fc1e18e7a
AaditT <aadit.trivedi03@gmail.com>
2019-03-02 11:21:54 -0800

    update

---
+++ creds.py
@@ -0,0 +1,4 @@
+consumer_key = "08YUWBp8lF6AkuDSOPp71LrgY"
+consumer_secret = "s1CbY5gqTMsBYrV5jpkgVivjIqfK05wmLVpa44otvziMsgnNhp"
+access_key = "1097354523432017920-mWlifS3sup5mWhZLBXXQKuIJ186rSn"
+access_secret = "Ad6z0qJPxDMr8rHIJGioJLUxofUTLEML6qva3R8Ip87BK"
