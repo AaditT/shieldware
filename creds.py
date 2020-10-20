0f198adbd843e3f07a3e396e56f9c7c9cd57ce91
Aadit Trivedi <aadit.trivedi03@gmail.com>
2020-10-20 15:20:14 -0700

    Delete creds.py

--- creds.py
+++
@@ -1,9 +0,0 @@
-# Twitter API
-consumer_key = "08YUWBp8lF6AkuDSOPp71LrgY"
-consumer_secret = "s1CbY5gqTMsBYrV5jpkgVivjIqfK05wmLVpa44otvziMsgnNhp"
-access_key = "1097354523432017920-mWlifS3sup5mWhZLBXXQKuIJ186rSn"
-access_secret = "Ad6z0qJPxDMr8rHIJGioJLUxofUTLEML6qva3R8Ip87BK"
-
-# SightEngine Client API
-client_access = "67608154"
-client_key = "756T67AahQBhkwg53TVy"
